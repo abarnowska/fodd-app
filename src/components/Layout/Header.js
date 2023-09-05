0e8c30951633c1fe25bc0294b6b6eacc1c8d322d
Angelika Barnowska-Nawara <angelika.barnowska@gmail.com>
2023-09-05 14:11:43 +0200

    first commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from "react";
+import classes from "./Header.module.css";
+import mealsImage from "../../assets/img/heroimg.jpg";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = props => {
+	return (
+		<Fragment>
+			<header className={classes.header}>
+				<h1>ReactMeals</h1>
+				<HeaderCartButton />
+			</header>
+			<div className={classes["main-image"]}>
+				<img src={mealsImage} alt='People sitting by table and eating.' />
+			</div>
+		</Fragment>
+	);
+};
+
+export default Header;
